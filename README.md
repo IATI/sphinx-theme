1dbe338152df632c477f9f36562b218a475661b6
Tilly Woodfield <22456167+tillywoodfield@users.noreply.github.com>
2024-04-17 12:55:55 +0300

    Run sphinx-quickstart

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ pip install -r requirements_dev.txt
 ### Update dependencies
 ```
 python -m piptools compile --extra=dev -o requirements_dev.txt pyproject.toml
+pip install -r requirements_dev.txt
 ```
 
 ### Run linting
@@ -19,3 +20,15 @@ black iati_sphinx_theme
 isort iati_sphinx_theme
 flake8 iati_sphinx_theme
 ```
+
+### Documentation with live preview
+
+1. Install the theme locally:
+    ```
+    pip install -e .
+    ```
+
+2. Start the docs development server:
+    ```
+    sphinx-autobuild docs docs/_build/html
+    ```
---
+++ docs/conf.py
@@ -0,0 +1,26 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "IATI Sphinx Theme"
+copyright = "2024 IATI Secretariat"
+author = "IATI Secretariat"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "iati_sphinx_theme"
+html_static_path = ["_static"]
