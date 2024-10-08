92d3843933fe466bc6326f8637a8ff5b9c24455b
Tilly Woodfield <22456167+tillywoodfield@users.noreply.github.com>
2024-10-08 11:27:00 +0300

    feat: make theme translatable

--- docs/conf.py
+++ docs/conf.py
@@ -3,12 +3,21 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+from os import path
+
+import sphinx.application
+from sphinx.locale import get_translation
+
+MESSAGE_CATALOG_NAME = "iati-sphinx-theme"
+_ = get_translation(MESSAGE_CATALOG_NAME)
+
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "IATI Sphinx Theme"
-copyright = "2024 IATI Secretariat"
 author = "IATI Secretariat"
+language = "en"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -21,9 +30,20 @@ extensions = [
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+locale_dirs = ["locale/"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "iati_sphinx_theme"
+html_theme_options = {
+    "header_title_text": _("Title"),
+}
+
+
 todo_include_todos = True
+
+
+def setup(app: sphinx.application.Sphinx) -> None:
+    locale_path = path.join(path.abspath(path.dirname(__file__)), "locale")
+    app.add_message_catalog(MESSAGE_CATALOG_NAME, locale_path)
--- iati_sphinx_theme/__init__.py
+++ iati_sphinx_theme/__init__.py
@@ -7,5 +7,9 @@ import sphinx.application
 
 def setup(app: sphinx.application.Sphinx) -> None:
     app.add_html_theme("iati_sphinx_theme", path.abspath(path.dirname(__file__)))
+
+    locale_path = path.join(path.abspath(path.dirname(__file__)), "locale")
+    app.add_message_catalog("iati-sphinx-theme", locale_path)
+
     app.config["html_permalinks_icon"] = "#"
     app.config["html_favicon"] = "static/favicon-16x16.png"
